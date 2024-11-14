42899ab600d642bc604f10178ef187fa72a9d541
22070462 <22070462@vnu.edu.vn>
2024-11-14 21:09:17 +0700

    Add model validation and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :subject
+
+  #validations
+  validates_presence_of :title, :author, :publisher
+  validates_uniqueness_of :title
+  validates_numericality_of :year
 end
+
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,5 @@
 class Subject < ApplicationRecord
     has_many :books
+
+    validates_presence_of :name
 end
